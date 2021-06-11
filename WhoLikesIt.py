5a1ca0af44388affdd83c8707fab82a70e9c975f
HighlySupreme <gal.1999k@gmail.com>
2021-06-11 13:54:08 +0200

    to je zel legit commit

--- WhoLikesIt.py
+++ WhoLikesIt.py
@@ -23,4 +23,20 @@ def _():
     test.assert_equals(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
     test.assert_equals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
 """
+#["Max", "John", "Mark"]
+def likes(names):
+    
+    if len(names) == 0: 
+        return "no one likes this"
+    if len(names) == 1: 
+        return "{} likes this".format(names[0]) 
+    if len(names) == 2: 
+        return "{} and {} like this".format(names[0], names[1])
+    if len(names) == 3: 
+        return "{}, {} and {} like this".format(names[0], names[1], names[2])
+    else:
+        return "{}, {} and {} others like this".format(names[0], names[1], len(names)-2)
+    
+   
 
+print(likes(['Max', 'John', 'Mark']))    
