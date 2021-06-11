6b60feb43acdeb7526613382f80fc9fea39cd99a
HighlySupreme <gal.1999k@gmail.com>
2021-06-11 14:09:32 +0200

    other version of code

--- WhoLikesIt.py
+++ WhoLikesIt.py
@@ -24,21 +24,33 @@ def _():
     test.assert_equals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
 """
 #["Max", "John", "Mark"]
-def likes(names):
+# def likes(names):
     
-    if len(names) == 0: 
-        return "no one likes this"
-    if len(names) == 1: 
-        return "{} likes this".format(names[0]) 
-    if len(names) == 2: 
-        return "{} and {} like this".format(names[0], names[1])
-    if len(names) == 3: 
-        return "{}, {} and {} like this".format(names[0], names[1], names[2])
-    else:
-        return "{}, {} and {} others like this".format(names[0], names[1], len(names)-2)
+#     if len(names) == 0: 
+#         return "no one likes this"
+#     if len(names) == 1: 
+#         return "{} likes this".format(names[0]) 
+#     if len(names) == 2: 
+#         return "{} and {} like this".format(names[0], names[1])
+#     if len(names) == 3: 
+#         return "{}, {} and {} like this".format(names[0], names[1], names[2])
+#     else:
+#         return "{}, {} and {} others like this".format(names[0], names[1], len(names)-2)
     
    
 
-print(likes(['Max', 'John', 'Mark']))    
+# print(likes(['Max', 'John', 'Mark']))    
 
-#testing .* commits
+#second solution
+
+
+def likes(names):
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this', 
+        2: '{} and {} like this', 
+        3: '{}, {} and {} like this', 
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n-2)
+
