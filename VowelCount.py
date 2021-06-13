ef5ff2fccc88f808408d3bbf659221cc5865def9
HighlySupreme <gal.1999k@gmail.com>
2021-06-13 10:37:32 +0200

    working

--- VowelCount.py
+++ VowelCount.py
@@ -1,10 +1,6 @@
 # Return the number (count) of vowels in the given string.
-
 # We will consider a, e, i, o, u as vowels for this Kata (but not y).
-
 # The input string will only consist of lower case letters and/or spaces.
-
-
 # import codewars_test as test
 
 # # for backward compatibility
@@ -23,10 +19,12 @@
 #         test.assert_equals(get_count("o a kak ushakov lil vo kashu kakao"), 13)
 #         test.assert_equals(get_count("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"), 168)
         
-
-
 def get_count(input_str):
     num_vowels = 0
+    vowels = "aeiou"
+    for x in input_str:
+        if x in vowels:
+            num_vowels += 1
 
     return num_vowels
 
