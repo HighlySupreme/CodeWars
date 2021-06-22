f2fa05fd24116bce9998aa8f22f3e92a7e67bb55
HighlySupreme <gal.1999k@gmail.com>
2021-06-22 11:08:52 +0200

    working

--- CreatePhone.py
+++ CreatePhone.py
@@ -13,4 +13,7 @@
 # test.assert_equals(create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), "(000) 000-0000")
 
 def create_phone_number(n):
-    #your code here
+    
+    return "({0}{1}{2} {3}{4}{5}-{6}{7}{8}{9})".format(n[0], n[1], n[2], n[3], n[4], n[5], n[6], n[7], n[8], n[9])
+
+create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
